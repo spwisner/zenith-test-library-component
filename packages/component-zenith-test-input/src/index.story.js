cc45e8e19ffffd4e4db647f872977d7b88f30e95
Steve Wisner <spwisner@gmail.com>
2018-09-11 19:53:22 -0400

    Update story

--- packages/component-zenith-test-input/src/index.story.js
+++ packages/component-zenith-test-input/src/index.story.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import ValidationInputComponent from '.'
+import ZenithValidationInput from '.'
 
-storiesOf('ValidationInputComponent', module)
+storiesOf('ZenithValidationInput', module)
 
   .add('Image Validation', () => (
     <ValidationInputComponent onChange={action('changed')} />
